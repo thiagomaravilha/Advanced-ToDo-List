3b8b9f6f28dfc04eac7fea4a99fca7f5690382b6
Thiago Maravilha <thiago.maravilha@outlook.com>
2025-05-05 15:04:35 -0300

    adicionado aviso de login invalido

--- imports/ui/LoginForm.jsx
+++ imports/ui/LoginForm.jsx
@@ -4,42 +4,49 @@ import React, { useState } from "react";
 export const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const submit = (e) => {
     e.preventDefault();
 
-    Meteor.loginWithPassword(username, password);
+    Meteor.loginWithPassword(username, password, (err) => {
+      if (err) {
+        setError("Login ou senha inválidos. Por favor, tente novamente.");
+      } else {
+        setError("");
+      }
+    });
   };
 
   return (
     <form onSubmit={submit} className="login-form">
-  <div>
-    <label htmlFor="username">Username</label>
+      {error && <p className="error-message">{error}</p>}
 
-    <input
-      type="text"
-      placeholder="Username"
-      name="username"
-      required
-      onChange={(e) => setUsername(e.target.value)}
-    />
-  </div>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
 
-  <div>
-    <label htmlFor="password">Password</label>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
 
-    <input
-      type="password"
-      placeholder="Password"
-      name="password"
-      required
-      onChange={(e) => setPassword(e.target.value)}
-    />
-  </div>
-
-  <div>
-    <button type="submit">Log In</button>
-  </div>
-</form>
+      <div>
+        <button type="submit">Log In</button>
+      </div>
+    </form>
   );
 };
